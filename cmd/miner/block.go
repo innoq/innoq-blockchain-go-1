ad3f0cf941999d3c62d66ced15974ac397faf115
Ruben Wagner <ruben.wagner@gmx.net>
2018-04-19 16:31:27 +0200

    merged all together

--- cmd/miner/block.go
+++ cmd/miner/block.go
@@ -1,18 +1,16 @@
 package main
 
-type Transactions struct {
+type Transaction struct {
 	Id        string `json:"id"`
 	Timestamp uint64 `json:"timestamp"`
 	Payload   string `json:"payload"`
 }
 type Block struct {
-	Index             uint64         `json:"index"`
-	Timestamp         int64          `json:"timestamp"`
-	Proof             uint64         `json:"proof"`
-	Transactions      []Transactions `json:"transactions"`
-	PreviousBlockHash string         `json:"previousBlockHash"`
+	Index             uint64        `json:"index"`
+	Timestamp         int64         `json:"timestamp"`
+	Proof             uint64        `json:"proof"`
+	Transactions      []Transaction `json:"transactions"`
+	PreviousBlockHash string        `json:"previousBlockHash"`
 }
 
 const GenesisBlock = `{"index":1,"timestamp":0,"proof":1917336,"transactions":[{"id":"b3c973e2-db05-4eb5-9668-3e81c7389a6d","timestamp":0,"payload":"I am Heribert Innoq"}],"previousBlockHash":"0"}`
-
-
--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -1,44 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
 	chain := NewChain()
+	miner := NewMiner(chain, "00")
 	overview := NewOverview(chain)
 
 	http.HandleFunc("/", overview.serveJson)
 
-	http.HandleFunc("/mine", mine)
+	http.HandleFunc("/mine", miner.mine)
 
-	http.HandleFunc("/blocks", blocks)
+	http.HandleFunc("/blocks", chain.serveJson)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func mine(w http.ResponseWriter, r *http.Request) {
-	// get last block
-	lastBlock := Block{} //lastBlock()
-	// verify hash of last block
-	lastBlockHash := hashBlock(lastBlock)
-
-	// create new block
-	nextBlock := Block{
-		Index:             lastBlock.Index + 1,
-		PreviousBlockHash: lastBlockHash,
-		Timestamp:         time.Now().Unix(),
-	}
-
-	// search for valid proof
-	nextBlock.Proof = generateProof(nextBlock, "00")
-	// add block
-	// AddBlock(nextBlock)
-
-	// return result
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
