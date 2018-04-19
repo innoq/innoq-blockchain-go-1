9662d226b1086f7d499e9dd4d37a1f921a3ed4c4
Christopher Schmidt <fakod666@googlemail.com>
2018-04-19 15:24:38 +0200

    1st version

---
+++ cmd/miner/mine.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"strings"
+)
+
+func generateProof(block Block, prefix string) uint64 {
+
+	var hash string
+
+	for n := uint64(0); strings.HasPrefix(hash, prefix); n++ {
+		block.Proof = n
+		str, _ := json.Marshal(block)
+		sum := sha256.Sum256([]byte(string(str)))
+		hash = hex.EncodeToString(sum[:])
+	}
+
+	return block.Proof
+}
