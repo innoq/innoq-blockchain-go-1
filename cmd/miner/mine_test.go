f0fdf158bb9c598ea01bd14c76e9e63908b9b918
GernotStarke@MadZorg42 <gs@gernotstarke.de>
2018-04-19 16:20:54 +0200

    refactored mine_test

--- cmd/miner/mine_test.go
+++ cmd/miner/mine_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 )
 
-func TestIrgendwas(t *testing.T) {
-	byt := []byte(`{"timestamp":0,"index":1,"proof":0,"transactions":[{"id":"b3c973e2-db05-4eb5-9668-3e81c7389a6d","timestamp":0,"payload":"I am Heribert Innoq"}],"previousBlockHash":"0"}`)
+func TestGenesisBlockYields1917336(t *testing.T) {
+
 	block := Block{}
-	if err := json.Unmarshal(byt, &block); err != nil {
+	if err := json.Unmarshal([]byte(GenesisBlock), &block); err != nil {
 		t.Fatal("Unmarshal error")
 	}
 	proof := generateProof(block, "000000")
