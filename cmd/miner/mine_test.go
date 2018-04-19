d826ae4f58e322f339438f090d39caaf9254e1de
Christopher Schmidt <fakod666@googlemail.com>
2018-04-19 16:15:25 +0200

    renamed

--- cmd/miner/mine_test.go
+++ cmd/miner/mine_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestIrgendwas(t *testing.T) {
+func TestGenerateProof(t *testing.T) {
 	byt := []byte(`{"timestamp":0,"index":1,"proof":0,"transactions":[{"id":"b3c973e2-db05-4eb5-9668-3e81c7389a6d","timestamp":0,"payload":"I am Heribert Innoq"}],"previousBlockHash":"0"}`)
 	block := Block{}
 	if err := json.Unmarshal(byt, &block); err != nil {
