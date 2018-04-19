146d7f10a725f4cfbad99f6d5c9c710ec294460c
Ruben Wagner <ruben.wagner@gmx.net>
2018-04-19 15:17:48 +0200

    impl overview

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -7,23 +7,14 @@ import (
 	"net/http"
 )
 
-type Overview struct {
-	NodeId             string `json:"nodeId"`
-	CurrentBlockHeight int64  `json:"currentBlockHeight"`
-}
-
 func main() {
-	http.HandleFunc("/", overview)
+	overview := NewOverview()
 
+	http.HandleFunc("/", overview.serveJson)
 	http.HandleFunc("/mine", mine)
-
 	http.HandleFunc("/blocks", blocks)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func overview(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
 func mine(w http.ResponseWriter, r *http.Request) {
