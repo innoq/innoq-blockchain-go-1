e478ede2933fe3c998169f2784a555da6acafb5e
GernotStarke@MadZorg42 <gs@gernotstarke.de>
2018-04-19 16:30:00 +0200

    alternative formulation of test: expected, actual

--- cmd/miner/mine_test.go
+++ cmd/miner/mine_test.go
@@ -11,9 +11,11 @@ func TestGenesisBlockYields1917336(t *testing.T) {
 	if err := json.Unmarshal([]byte(GenesisBlock), &block); err != nil {
 		t.Fatal("Unmarshal error")
 	}
-	proof := generateProof(block, "000000")
-	t.Log(proof)
-	if proof != 1917336 {
-		t.Errorf("fail")
+	
+	actual, expected := generateProof(block, "000000"), uint64(1917336)
+
+	if actual != expected {
+		t.Errorf("% q != %q", actual, expected)
 	}
+
 }
