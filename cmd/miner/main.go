5e3a6e691dd5781bfc649b57c9e6f585a7264925
Simon Jost <simon.jost@innoq.com>
2018-04-19 12:50:30 +0200

    save

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		b := Block{Index: "1", Timestamp: "0", Proof: 0, Transaction: []string{}, PreviousBlockHash: "0"}
+		json.NewEncoder(w).Encode(b)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func hash(block Block) {
+	str, _ := json.Marshall(block)
 }
