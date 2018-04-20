282a6246d2a26c436fbe38c16dcf831c83cfd3b6
Ruben Wagner <ruben.wagner@gmx.net>
2018-04-20 15:00:02 +0200

    more explicit routing

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -24,19 +24,19 @@ func main() {
 	miner.Start()
 	defer miner.Stop()
 
-	r.HandleFunc("/", overview.serveJson)
+	r.HandleFunc("/", overview.serveJson).Methods(http.MethodGet)
 
-	r.HandleFunc("/mine", miner.mine)
+	r.HandleFunc("/mine", miner.mine).Methods(http.MethodGet)
 
-	r.HandleFunc("/blocks", chain.serveJson)
+	r.HandleFunc("/blocks", chain.serveJson).Methods(http.MethodGet)
 
-	r.Handle("/events", events)
+	r.Handle("/events", events).Methods(http.MethodGet)
 
-	r.HandleFunc("/transactions", transactions.Post)
+	r.HandleFunc("/transactions", transactions.Post).Methods(http.MethodGet, http.MethodPost)
 
-	r.HandleFunc("/transactions/{id}", transactions.serveJson)
+	r.HandleFunc("/transactions/{id}", transactions.serveJson).Methods(http.MethodGet)
 
-	r.HandleFunc("/ui", GetIndex)
+	r.HandleFunc("/ui", GetIndex).Methods(http.MethodGet)
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
